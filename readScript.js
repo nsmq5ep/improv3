1143b4f6b3af2986cb4e7c2b0734f0baa610bd33
Morris Vane <thetabuns@gmx.com>
2023-12-28 22:09:49 +0530

    _22_09_28_12_2023

--- readScript.js
+++ readScript.js
@@ -1,16 +1,14 @@
 // readScript.js
 
-$(document).ready(function(){
-    $("#getData").click(function(){
-        $.ajax({
-            url: 'https://api.jsonstorage.net/v1/json/86d2965f-cae1-43a2-8f49-cb2da6a5527f/e430f896-84a1-4ddb-a1f8-bedcfb5c1561',
-            type: 'GET',
-            success: function(result) {
-                console.log(result);
-            },
-            error: function(error){
-                console.log(error);
-            }
-        });
+function getJSONData() {
+    $.ajax({
+        url: 'https://api.jsonstorage.net/v1/json/86d2965f-cae1-43a2-8f49-cb2da6a5527f/e430f896-84a1-4ddb-a1f8-bedcfb5c1561',
+        type: 'GET',
+        success: function(result) {
+            $('#jsonText').val(JSON.stringify(result));
+        },
+        error: function(error){
+            console.log(error);
+        }
     });
-});
+}
